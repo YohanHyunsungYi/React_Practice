a89f74e4c5f45f5105ba3ea70f869dca916eb2fb
Yohan Hyunsung Yi <hyunsungyi@gmail.com>
2017-10-17 19:40:56 -0700

    wired

---
+++ src/components/video_list_item.js
@@ -0,0 +1,23 @@
+
+/*    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageURL} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+      </div>
+    </li>
+*/
+
+import React from 'react';
+
+const VideoListItem = (props) => {
+  return <li>Video</li>;
+};
+
+export default VideoListItem;
